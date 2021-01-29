cccd230b988454a39306aa0cc5325d0de2f6013e
Chris Su <cs.akasuv@gmail.com>
2021-01-29 18:05:39 +0800

    feature: update prettier config

--- config/prettier.js
+++ config/prettier.js
@@ -1,4 +1,14 @@
 module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
   overrides: [
     {
       files: "*.wxml",
